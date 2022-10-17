95d04e28f3b6bd133967bd23ac37b3497dcd37da
kyle jenne <strongagent1@gmail.com>
2022-10-17 16:59:34 -0400

    Operator exercises for KJ

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // for division, operator exercise
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+        //determine the area of the user entered circle
+        public static double AreaOfCircle(double radius)
+        {
+            //scope of the method
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
         }
     }
 }
